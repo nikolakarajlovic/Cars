461c45d42d4e48b15d28516a03df24330e6c4a6a
VivifyIdeas <vivify@VivifyIdeass-MacBook-Pro-8.local>
2022-02-10 21:24:36 +0100

    commit 5

---
+++ resources/views/car.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <h1>Car:</h1>
+        
+            <p>{{$car->producer}},
+                {{$car->title}},
+                {{$car->number_of_doors}} doors.</p>
+    </body>
+</html>
